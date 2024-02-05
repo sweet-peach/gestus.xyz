647573366b7ca9e5402e1c6e7dc2e6d2ffc3ddf3
sweet-peach <you@example.com>
2024-02-05 12:38:13 +0100

    Barebones server first commit

---
+++ server/src/main/java/xyz/gestus/gestus/GestusApplication.java
@@ -0,0 +1,13 @@
+package xyz.gestus.gestus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestusApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/xyz/gestus/gestus/GestusApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.gestus.gestus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
