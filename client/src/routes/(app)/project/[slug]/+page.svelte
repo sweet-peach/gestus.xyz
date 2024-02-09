35b296d460f469170ff6115f0a4adba98f59d611
sweet-peach <nikita.oleynikov@ciclosformativosceu.es>
2024-02-09 17:02:00 +0100

    Files view structure described

--- client/src/routes/(app)/project/[slug]/+page.svelte
+++ client/src/routes/(app)/project/[slug]/+page.svelte
@@ -7,6 +7,7 @@
 
     onMount(async () => {
         $files = await getFilesByProjectId(2)
+        console.log($files);
     })
 </script>
 
@@ -27,18 +28,89 @@
     </div>
     <div class="files-view">
         <div class="file-header">
-            <div className="name">Name</div>
-            <div className="date">Date</div>
-            <div className="size">File size</div>
+            <div class="name">Name</div>
+            <div class="date">Date</div>
+            <div class="size">File size</div>
         </div>
-        {#each $files as {id, name}}
-            <div class="file">
-                {id}
+        {#each $files as {date, name, size, type}}
+            <div class="file-list">
+                <div class="icon">
+                    {#if type === "dir"}
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                             class="bi bi-folder-fill" viewBox="0 0 16 16">
+                            <path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a2 2 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3m-8.322.12q.322-.119.684-.12h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981z"/>
+                        </svg>
+                    {:else}
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-fill" viewBox="0 0 16 16">
+                            <path d="M4 0h5.293A1 1 0 0 1 10 .293L13.707 4a1 1 0 0 1 .293.707V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2m5.5 1.5v2a1 1 0 0 0 1 1h2z"/>
+                        </svg>
+                    {/if}
+                </div>
+                <div class="name">
+                    {name}
+                </div>
+                <div class="date">
+                    {String(date).toString().slice(0,10)}
+                </div>
+                <div class="size">
+                    {#if type !== "dir"}
+                        {size}
+                    {/if}
+                </div>
+                <div class="action">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                         class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                        <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
+                    </svg>
+                </div>
             </div>
         {/each}
     </div>
 </div>
 
-<style></style>
+<style lang="scss">
+  .file-list {
+    display: grid;
+    grid-template-columns: 1fr 4fr repeat(3, 1fr);
+
+    .name {
+      grid-column-start: 2;
+    }
+
+    .date {
+      grid-column-start: 3;
+      justify-self: center;
+    }
+
+    .size {
+      grid-column-start: 4;
+      justify-self: center;
+    }
+
+    .action {
+      grid-column-start: 5;
+      justify-self: center;
+    }
+  }
+
+  .file-header {
+    display: grid;
+    grid-template-columns: 1fr 4fr repeat(3, 1fr);
+
+    .name {
+      grid-column-start: 2;
+    }
+
+    .date {
+      grid-column-start: 3;
+      justify-self: center;
+    }
+
+    .size {
+      grid-column-start: 4;
+      justify-self: center;
+    }
+  }
+</style>
 
 
