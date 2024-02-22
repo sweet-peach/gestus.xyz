0a675e1fd769cc81a89775a067ca2f97bfae221c
sweet-peach <akjevfqkjdrtqdfq@mail.com>
2024-02-22 17:58:39 +0100

    Finished the keywords logic
    Improve keyword display and management in form

--- client/src/components/ProjectFormModal/Items/Keywords.svelte
+++ client/src/components/ProjectFormModal/Items/Keywords.svelte
@@ -1,32 +1,56 @@
 <script>
-import {formData} from "$lib/stores/projectFormStore.js";
+    import {formData} from "$lib/stores/projectFormStore.js";
 
-export let title="";
-export let keywords = [];
+    export let title = "";
+    export let keywords = [];
 
-let currentKeyword = "";
-$: if (currentKeyword) {
-    const lastChar = currentKeyword[currentKeyword.length - 1];
-    if (lastChar === ",") {
-        currentKeyword = currentKeyword.slice(0, currentKeyword.length - 1);
+    let input;
+    let error = false;
 
+    let currentKeyword = "";
+    $: if (currentKeyword) {
+        error = false;
+        const lastChar = currentKeyword[currentKeyword.length - 1];
+        if (lastChar === ",") {
+            currentKeyword = currentKeyword.slice(0, currentKeyword.length - 1);
+
+            if (!$formData.keywords.includes(currentKeyword)) {
+                formData.update((data) => {
+                    data.keywords.push(currentKeyword);
+                    return data;
+                })
+                currentKeyword = "";
+            } else {
+                error = true;
+            }
+        }
+    }
+
+    function deleteKeyword(keyword) {
         formData.update((data) => {
-            data.keywords.push(currentKeyword);
+            data.keywords = data.keywords.filter((k) => k !== keyword);
             return data;
         })
-        currentKeyword = "";
     }
-}
+
+    function focus() {
+        input.focus();
+    }
 </script>
 
 <div class="item">
-    <h3>{title}</h3>
-    <div class="keywords-input">
-        <div class="keywords">
+    <header>
+        <h3>{title}</h3>
+        {#if error}
+            <span class="error-text">This keyword already exist</span>
+        {/if}
+    </header>
+    <div on:click={focus} tabindex="0" class="keywords-input" role="button" aria-label={`Focus`} on:keypress={focus}>
+        <div class="keywords" aria-label="Keywords list">
             {#each keywords as keyword}
-                <div class="keyword">
+                <div on:click|stopPropagation role="none" class="keyword">
                     {keyword}
-                    <button on:click={()=> {}}>
+                    <button on:click={()=> { deleteKeyword(keyword) }}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 384 512"
                              fill="currentColor">
                             <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
@@ -34,8 +58,8 @@ $: if (currentKeyword) {
                     </button>
                 </div>
             {/each}
+            <input bind:this={input} bind:value={currentKeyword} type="text">
         </div>
-        <input bind:value={currentKeyword} type="text">
     </div>
     <p class="hint">Use the comma "," as a separator</p>
 </div>
